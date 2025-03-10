59a5973fb4ab5bea00b9e4ff935724af9ed3d2b4
Xiaoxi Yang <yang0407@algonquinlive.com>
2025-03-10 18:30:58 -0400

    ADD TEXT FOR BRANCH 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//ADD SOMETHING IN BRANCH2 FROM XIAOXI
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
